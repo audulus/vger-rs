505b9a6701b470abd5f46dfab01bfa33e255e40d
Taylor Holliday <taylor@audulus.com>
2022-01-22 18:46:06 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,17 @@ pub struct VGER {
     pub device: wgpu::Device,
 }
 
+impl VGER {
+
+    /*
+    fn new() -> Self {
+        let backend = wgpu::Backends::all();
+        let instance = wgpu::Instance::new(backend);
+    }
+    */
+    
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
