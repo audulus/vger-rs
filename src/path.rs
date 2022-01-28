ab594657cc9cb2bd7680d59f8729206285b19aaf
Taylor Holliday <taylor@audulus.com>
2022-01-28 13:32:00 -0800

    Update path.rs

--- src/path.rs
+++ src/path.rs
@@ -1,7 +1,13 @@
 
+use std::cmp::Ord;
 use euclid::*;
 pub struct WorldSpace;
 pub type WorldPoint = Point2D<f32, WorldSpace>;
+
+struct Interval {
+    a: f32,
+    b: f32
+}
 
 struct PathSegment {
     cvs: [WorldPoint; 3],
@@ -9,6 +15,16 @@ struct PathSegment {
     previous: i32
 }
 
+impl PathSegment {
+    pub fn y_interval(&self) -> Interval {
+        Interval {
+            a: self.cvs[0].y.min(self.cvs[1].y).min(self.cvs[2].y),
+            b: self.cvs[0].y.min(self.cvs[1].y).max(self.cvs[2].y)
+        }
+    }
+}
+
+#[derive(PartialEq, PartialOrd)]
 struct PathScannerNode {
     coord: f32,
     seg: i32,
@@ -17,14 +33,16 @@ struct PathScannerNode {
 
 pub struct PathScanner {
     segments: Vec<PathSegment>,
-    nodes: Vec<PathScannerNode>
+    nodes: Vec<PathScannerNode>,
+    index: i32
 }
 
 impl PathScanner {
     pub fn new() -> Self {
         Self {
             segments: vec![],
-            nodes: vec![]
+            nodes: vec![],
+            index: 0
         }
     }
 
@@ -56,5 +74,21 @@ impl PathScanner {
                 }
             }
         }
+
+        self.nodes.clear();
+        self.index = 0;
+
+        for i in 0..self.segments.len() {
+            let y_interval = self.segments[i].y_interval();
+            self.nodes.push(PathScannerNode {
+                coord: y_interval.a, seg: i as i32, end: false
+            });
+            self.nodes.push(PathScannerNode {
+                coord: y_interval.b, seg: i as i32, end: true
+            });
+        }
+        
+        self.nodes.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
     }
 }
