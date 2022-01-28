ab732e1e1ed923d56431e1e5f53b4fef55168be8
Taylor Holliday <taylor@audulus.com>
2022-01-28 13:17:46 -0800

    Update path.rs

--- src/path.rs
+++ src/path.rs
@@ -3,20 +3,28 @@ use euclid::*;
 pub struct WorldSpace;
 pub type WorldPoint = Point2D<f32, WorldSpace>;
 
-pub struct PathSegment {
+struct PathSegment {
     cvs: [WorldPoint; 3],
     next: i32,
     previous: i32
 }
 
+struct PathScannerNode {
+    coord: f32,
+    seg: i32,
+    end: bool
+}
+
 pub struct PathScanner {
-    segments: Vec<PathSegment>
+    segments: Vec<PathSegment>,
+    nodes: Vec<PathScannerNode>
 }
 
 impl PathScanner {
     pub fn new() -> Self {
         Self {
-            segments: vec![]
+            segments: vec![],
+            nodes: vec![]
         }
     }
 
