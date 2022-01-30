5f65597843328bfd11d651b4edb80314968d7a44
Taylor Holliday <taylor@audulus.com>
2022-01-30 08:45:33 -0800

    Update scene.rs

--- src/scene.rs
+++ src/scene.rs
@@ -24,21 +24,59 @@ pub struct Scene {
     pub prims: [GPUVec<Prim>; MAX_LAYERS],
     pub xforms: GPUVec<LocalToWorld>,
     pub paints: GPUVec<Paint>,
+    pub bind_group: wgpu::BindGroup
 }
 
 pub const MAX_PRIMS: usize = 65536;
 
 impl Scene {
     pub fn new(device: &wgpu::Device) -> Self {
-        Self {
-            prims: [
-                GPUVec::new(device, MAX_PRIMS, "Prim Buffer 0"),
-                GPUVec::new(device, MAX_PRIMS, "Prim Buffer 1"),
-                GPUVec::new(device, MAX_PRIMS, "Prim Buffer 2"),
-                GPUVec::new(device, MAX_PRIMS, "Prim Buffer 3"),
+
+        let prims = [
+            GPUVec::new(device, MAX_PRIMS, "Prim Buffer 0"),
+            GPUVec::new(device, MAX_PRIMS, "Prim Buffer 1"),
+            GPUVec::new(device, MAX_PRIMS, "Prim Buffer 2"),
+            GPUVec::new(device, MAX_PRIMS, "Prim Buffer 3"),
+        ];
+
+        let bind_group_layout = device.create_bind_group_layout(
+            &wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage{read_only: false},
+                            has_dynamic_offset: true,
+                            min_binding_size: None
+                        },
+                        count: None,
+                    },
+                ],
+                label: Some("bind_group_layout"),
+            }
+        );
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding{
+                        buffer: prims[0].buffer(),
+                        offset: 0,
+                        size: None
+                    }),
+                },
             ],
+            label: Some("vger bind group"),
+        });
+
+        Self {
+            prims,
             xforms: GPUVec::new(device, MAX_PRIMS, "Xform Buffer"),
             paints: GPUVec::new(device, MAX_PRIMS, "Paint Buffer"),
+            bind_group
         }
     }
 }
