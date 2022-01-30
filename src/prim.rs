d51fb0c2cd70da17d688250663245b926aaf9c15
Taylor Holliday <taylor@audulus.com>
2022-01-29 22:13:50 -0800

    Update prim.rs

--- src/prim.rs
+++ src/prim.rs
@@ -46,35 +46,35 @@ pub type LocalPoint = Point2D<f32, LocalSpace>;
 #[derive(Copy, Clone, Default)]
 pub struct Prim {
     /// Type of primitive.
-    prim_type: PrimType,
+    pub prim_type: PrimType,
 
     /// Stroke width.
-    width: f32,
+    pub width: f32,
 
     /// Radius of circles. Corner radius for rounded rectangles.
-    radius: f32,
+    pub radius: f32,
 
     /// Control vertices.
-    cvs: [f32; 6],
+    pub cvs: [LocalPoint; 3],
 
     /// Start of the control vertices, if they're in a separate buffer.
-    start: u32,
+    pub start: u32,
 
     /// Number of control vertices (vgerCurve and vgerPathFill)
-    count: u32,
+    pub count: u32,
 
     /// Index of paint applied to drawing region.
-    paint: u32,
+    pub paint: u32,
 
     /// Glyph region index. (used internally)
-    glyph: u32,
+    pub glyph: u32,
 
     /// Index of transform applied to drawing region. (used internally)
-    xform: u32,
+    pub xform: u32,
 
     /// Min and max coordinates of the quad we're rendering. (used internally)
-    quad_bounds: [f32; 4],
+    pub quad_bounds: [f32; 4],
 
     /// Min and max coordinates in texture space. (used internally)
-    tex_bounds: [f32; 4],
+    pub tex_bounds: [f32; 4],
 }
