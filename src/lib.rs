9901d05779e0e0d6840fead68dfcad681c286d77
Taylor Holliday <taylor@audulus.com>
2022-01-29 16:42:08 -0800

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -15,67 +15,15 @@ use std::mem::size_of;
 mod scene;
 use scene::*;
 
-struct LocalSpace {}
-type LocalToWorld = Transform2D<f32, LocalSpace, WorldSpace>;
+mod prim;
 
 pub struct VGER {
     pub device: wgpu::Device,
     pub queue: wgpu::Queue,
-    pub prim_buffer: wgpu::Buffer,
-    pub xform_buffer: wgpu::Buffer,
-    pub paint_buffer: wgpu::Buffer
+    pub scene: Scene
 }
 
 const MAX_PRIMS: usize = 65536;
-
-struct Prim {
-
-    /// Type of primitive.
-    prim_type: u32,
-
-    /// Stroke width.
-    width: f32,
-
-    /// Radius of circles. Corner radius for rounded rectangles.
-    radius: f32,
-
-    /// Control vertices.
-    cvs: [f32; 6],
-
-    /// Start of the control vertices, if they're in a separate buffer.
-    start: u32,
-
-    /// Number of control vertices (vgerCurve and vgerPathFill)
-    count: u32,
-
-    /// Index of paint applied to drawing region.
-    paint: u32,
-
-    /// Glyph region index. (used internally)
-    glyph: u32,
-
-    /// Index of transform applied to drawing region. (used internally)
-    xform: u32,
-
-    /// Min and max coordinates of the quad we're rendering. (used internally)
-    quad_bounds: [f32; 4],
-
-    /// Min and max coordinates in texture space. (used internally)
-    tex_bounds: [f32; 4]
-
-}
-
-struct Paint {
-
-    xform: LocalToWorld,
-
-    inner_color: [f32; 4],
-    outer_color: [f32; 4],
-
-    glow: f32,
-    image: i32,
-
-}
 
 impl VGER {
 
@@ -114,34 +62,9 @@ impl VGER {
             source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(include_str!("shader.wgsl"))),
         });
 
-        let prim_buffer = device.create_buffer(
-            &wgpu::BufferDescriptor {
-                label: Some("Prim Buffer"),
-                size: (MAX_PRIMS * size_of::<Prim>()) as u64,
-                usage: BufferUsages::MAP_WRITE,
-                mapped_at_creation: true
-            }
-        );
+        let scene = Scene::new(&device);
 
-        let xform_buffer = device.create_buffer(
-            &wgpu::BufferDescriptor {
-                label: Some("Xform Buffer"),
-                size: (MAX_PRIMS * size_of::<LocalToWorld>()) as u64,
-                usage: BufferUsages::MAP_WRITE,
-                mapped_at_creation: true
-            }
-        );
-
-        let paint_buffer = device.create_buffer(
-            &wgpu::BufferDescriptor {
-                label: Some("Paint Buffer"),
-                size: (MAX_PRIMS * size_of::<Paint>()) as u64,
-                usage: BufferUsages::MAP_WRITE,
-                mapped_at_creation: true
-            }
-        );
-
-        Self { device, queue, prim_buffer, xform_buffer, paint_buffer }
+        Self { device, queue, scene }
     }
 
 }
---
+++ src/prim.rs
@@ -0,0 +1,38 @@
+
+
+pub struct Prim {
+
+    /// Type of primitive.
+    prim_type: u32,
+
+    /// Stroke width.
+    width: f32,
+
+    /// Radius of circles. Corner radius for rounded rectangles.
+    radius: f32,
+
+    /// Control vertices.
+    cvs: [f32; 6],
+
+    /// Start of the control vertices, if they're in a separate buffer.
+    start: u32,
+
+    /// Number of control vertices (vgerCurve and vgerPathFill)
+    count: u32,
+
+    /// Index of paint applied to drawing region.
+    paint: u32,
+
+    /// Glyph region index. (used internally)
+    glyph: u32,
+
+    /// Index of transform applied to drawing region. (used internally)
+    xform: u32,
+
+    /// Min and max coordinates of the quad we're rendering. (used internally)
+    quad_bounds: [f32; 4],
+
+    /// Min and max coordinates in texture space. (used internally)
+    tex_bounds: [f32; 4]
+
+}
