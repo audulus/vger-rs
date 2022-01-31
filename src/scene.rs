55d4f0439b951d0af4711e1c9ea87780271b13ef
Taylor Holliday <taylor@audulus.com>
2022-01-30 21:10:38 -0800

    Update scene.rs

--- src/scene.rs
+++ src/scene.rs
@@ -11,6 +11,7 @@ pub const MAX_LAYERS: usize = 4;
 
 pub struct Scene {
     pub prims: [GPUVec<Prim>; MAX_LAYERS],
+    pub cvs: GPUVec<LocalPoint>,
     pub xforms: GPUVec<LocalToWorld>,
     pub paints: GPUVec<Paint>,
     pub bind_groups: [wgpu::BindGroup; MAX_LAYERS],
@@ -27,18 +28,20 @@ impl Scene {
             GPUVec::new(device, MAX_PRIMS, "Prim Buffer 3"),
         ];
 
+        let cvs = GPUVec::new(device, MAX_PRIMS, "cv Buffer");
         let xforms = GPUVec::new(device, MAX_PRIMS, "Xform Buffer");
         let paints = GPUVec::new(device, MAX_PRIMS, "Paint Buffer");
 
         let bind_groups = [
-            Scene::bind_group(device, &prims[0], &xforms, &paints),
-            Scene::bind_group(device, &prims[1], &xforms, &paints),
-            Scene::bind_group(device, &prims[2], &xforms, &paints),
-            Scene::bind_group(device, &prims[3], &xforms, &paints),
+            Scene::bind_group(device, &prims[0], &cvs, &xforms, &paints),
+            Scene::bind_group(device, &prims[1], &cvs, &xforms, &paints),
+            Scene::bind_group(device, &prims[2], &cvs, &xforms, &paints),
+            Scene::bind_group(device, &prims[3], &cvs, &xforms, &paints),
         ];
 
         Self {
             prims,
+            cvs,
             xforms,
             paints,
             bind_groups,
@@ -48,6 +51,7 @@ impl Scene {
     fn bind_group(
         device: &wgpu::Device,
         prims: &GPUVec<Prim>,
+        cvs: &GPUVec<LocalPoint>,
         xforms: &GPUVec<LocalToWorld>,
         paints: &GPUVec<Paint>
     ) -> wgpu::BindGroup {
@@ -82,6 +86,16 @@ impl Scene {
                         min_binding_size: None,
                     },
                     count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 3,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: false },
+                        has_dynamic_offset: true,
+                        min_binding_size: None,
+                    },
+                    count: None,
                 }
             ],
             label: Some("bind_group_layout"),
@@ -101,13 +115,21 @@ impl Scene {
                 wgpu::BindGroupEntry {
                     binding: 1,
                     resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
-                        buffer: xforms.buffer(),
+                        buffer: cvs.buffer(),
                         offset: 0,
                         size: None,
                     }),
                 },
                 wgpu::BindGroupEntry {
                     binding: 2,
+                    resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
+                        buffer: xforms.buffer(),
+                        offset: 0,
+                        size: None,
+                    }),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 3,
                     resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
                         buffer: paints.buffer(),
                         offset: 0,
