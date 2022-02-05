ff9009388a6eccd1f05b9d180de0b141eb398593
Taylor Holliday <taylor@audulus.com>
2022-02-04 19:03:49 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -594,7 +594,6 @@ mod tests {
 
         vger.begin(512.0, 512.0, 1.0);
         
-        // vgerLinearGradient(vger, float2{50,450}, float2{100,450}, cyan, magenta, 0)
         let paint = vger.linear_gradient([100.0,100.0].into(), [200.0,200.0].into(), Color::CYAN, Color::MAGENTA, 0.0);
 
         vger.stroke_rect([100.0,100.0].into(), [200.0,200.0].into(), 10.0, 4.0, paint);
