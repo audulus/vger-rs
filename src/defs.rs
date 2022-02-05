300389470aa51256ed9782637bed5258b2b5953e
Taylor Holliday <taylor@audulus.com>
2022-02-04 19:11:14 -0800

    Update defs.rs

--- src/defs.rs
+++ src/defs.rs
@@ -12,9 +12,3 @@ pub type LocalVector = Vector2D<f32, LocalSpace>;
 
 pub type LocalToWorld = Transform2D<f32, LocalSpace, WorldSpace>;
 pub type WorldToLocal = Transform2D<f32, WorldSpace, LocalSpace>;
-
-pub fn to_mat3x2<A, B>(xform: Transform2D<f32, A, B>) -> [f32; 8] {
-    [
-        xform.m11, xform.m21, xform.m31, 0.0, xform.m12, xform.m22, xform.m32, 0.0,
-    ]
-}
