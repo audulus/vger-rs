b75b8ceafed604382578740ec820e20f5a389ce6
Taylor Holliday <taylor@audulus.com>
2022-02-07 09:21:40 -0800

    Update atlas.rs

--- src/atlas.rs
+++ src/atlas.rs
@@ -3,31 +3,46 @@ use wgpu;
 use wgpu::util::DeviceExt;
 use rectangle_pack::*;
 // use std::collections::BTreeMap;
+use rect_packer::Packer;
 
 struct Atlas {
-    rects: GroupedRectsToPlace<u32>,
+    packer: Packer,
     new_data: Vec<wgpu::Buffer>
 }
 
 impl Atlas {
 
+    pub fn new() -> Self {
+
+        let config = rect_packer::Config {
+            width: 1024,
+            height: 1024,
+        
+            border_padding: 5,
+            rectangle_padding: 10,
+        };
+
+        Self {
+            packer: Packer::new(config),
+            new_data: vec![],
+        }
+
+    }
+
     pub fn add_region(&mut self, device: &wgpu::Device, data: &[u8], width: u32, height: u32) {
 
-        let buffer = device.create_buffer_init(
-            &wgpu::util::BufferInitDescriptor {
-                label: Some("Temp Buffer"),
-                contents: &data,
-                usage: wgpu::BufferUsages::COPY_SRC,
-            }
-        );
+        if let Some(rect) = self.packer.pack(width as i32, height as i32, false) {
 
-        self.rects.push_rect(
-            self.new_data.len() as u32,
-            None,
-            RectToInsert::new(width, height, 0)
-        );
+            let buffer = device.create_buffer_init(
+                &wgpu::util::BufferInitDescriptor {
+                    label: Some("Temp Buffer"),
+                    contents: &data,
+                    usage: wgpu::BufferUsages::COPY_SRC,
+                }
+            );
 
-        self.new_data.push(buffer);
+            self.new_data.push(buffer);
+        }        
 
     }
 }
