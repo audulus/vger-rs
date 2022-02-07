a676e14d30e736a3486f276a1d34101c02db2047
Taylor Holliday <taylor@audulus.com>
2022-02-07 09:28:03 -0800

    Update atlas.rs

--- src/atlas.rs
+++ src/atlas.rs
@@ -4,9 +4,16 @@ use wgpu::util::DeviceExt;
 // use std::collections::BTreeMap;
 use rect_packer::Packer;
 
-struct Atlas {
+#[derive(Debug)]
+struct ImageData {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+}
+
+pub struct Atlas {
     packer: Packer,
-    new_data: Vec<wgpu::Buffer>,
+    new_data: Vec<ImageData>,
 }
 
 impl Atlas {
@@ -25,15 +32,28 @@ impl Atlas {
         }
     }
 
-    pub fn add_region(&mut self, device: &wgpu::Device, data: &[u8], width: u32, height: u32) {
-        if let Some(rect) = self.packer.pack(width as i32, height as i32, false) {
-            let buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-                label: Some("Temp Buffer"),
-                contents: &data,
-                usage: wgpu::BufferUsages::COPY_SRC,
-            });
+    pub fn add_region(&mut self, data: &[u8], width: u32, height: u32) {
+        self.new_data.push(ImageData {
+            width,
+            height,
+            data: data.into(),
+        });
+    }
 
-            self.new_data.push(buffer);
+    pub fn update(&mut self, device: &wgpu::Device, queue: &wgpu::Queue) {
+        for data in &self.new_data {
+            if let Some(rect) = self
+                .packer
+                .pack(data.width as i32, data.height as i32, false)
+            {
+                let buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some("Temp Buffer"),
+                    contents: &data.data,
+                    usage: wgpu::BufferUsages::COPY_SRC,
+                });
+            }
         }
+
+        self.new_data.clear();
     }
 }
