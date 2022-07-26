319677369e2ebe315ab4ff4165c7092a14bdb022
Taylor Holliday <taylor@audulus.com>
2022-07-26 15:06:40 -0700

    Update tests.rs

--- tests/tests.rs
+++ tests/tests.rs
@@ -538,7 +538,7 @@ fn test_scissor_text() {
     let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     vger.translate([32.0, 256.0]);
-    vger.scissor(euclid::rect(0.0, 0.0, 100.0, 100.0));
+    vger.scissor(euclid::rect(-100.0, -100.0, 400.0, 400.0));
 
     let bounds = vger.text_bounds(lorem, 18, Some(448.0));
 
