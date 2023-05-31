d897334b93155629d90013407d2f860697bde8ba
Taylor Holliday <wtholliday@gmail.com>
2023-05-30 18:55:32 -0700

    Reuse BindGroupLayout

--- src/scene.rs
+++ src/scene.rs
@@ -30,13 +30,13 @@ impl Scene {
         let paints = GPUVec::new(device, MAX_PRIMS, "Paint Buffer");
         let scissors = GPUVec::new(device, MAX_PRIMS, "scissor Buffer");
 
-        //let bind_group_layout = Self::bind_group_layout(device);
+        let bind_group_layout = Self::bind_group_layout(device);
 
         let bind_groups = [
-            Scene::bind_group(device, &prims[0], &cvs, &xforms, &paints, &scissors),
-            Scene::bind_group(device, &prims[1], &cvs, &xforms, &paints, &scissors),
-            Scene::bind_group(device, &prims[2], &cvs, &xforms, &paints, &scissors),
-            Scene::bind_group(device, &prims[3], &cvs, &xforms, &paints, &scissors),
+            Scene::bind_group(device, &bind_group_layout, &prims[0], &cvs, &xforms, &paints, &scissors),
+            Scene::bind_group(device, &bind_group_layout, &prims[1], &cvs, &xforms, &paints, &scissors),
+            Scene::bind_group(device, &bind_group_layout, &prims[2], &cvs, &xforms, &paints, &scissors),
+            Scene::bind_group(device, &bind_group_layout, &prims[3], &cvs, &xforms, &paints, &scissors),
         ];
 
         Self {
@@ -65,13 +65,13 @@ impl Scene {
 
     fn bind_group(
         device: &wgpu::Device,
+        bind_group_layout: &wgpu::BindGroupLayout,
         prims: &GPUVec<Prim>,
         cvs: &GPUVec<LocalPoint>,
         xforms: &GPUVec<Mat4x4>,
         paints: &GPUVec<Paint>,
         scissors: &GPUVec<Scissor>,
     ) -> wgpu::BindGroup {
-        let bind_group_layout = Self::bind_group_layout(device);
 
         device.create_bind_group(&wgpu::BindGroupDescriptor {
             layout: &bind_group_layout,
