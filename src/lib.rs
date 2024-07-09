90cc8fee949b9c2d6c806f78d345d572faa16381
Mark Friedenbach <mark@friedenbach.org>
2024-07-08 23:48:23 -0700

    Update wgpu 0.19 -> 0.20

--- src/lib.rs
+++ src/lib.rs
@@ -228,11 +228,13 @@ impl Vger {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main",
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
                 buffers: &[],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
                 entry_point: "fs_main",
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
                 targets: &[Some(wgpu::ColorTargetState {
                     format: texture_format,
                     blend: Some(wgpu::BlendState {
